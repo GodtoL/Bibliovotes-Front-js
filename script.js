07cd5592b0458173f6eb60b9b3acd906e097672d
GodtoL <eliasvera793@gmail.com>
2024-12-30 21:45:32 -0300

    Se agrega la visualización de los comentarios

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Simulación de una lista de enlaces (puedes reemplazarlo con datos reales)
 const books = [
     {
-        "id": 34,
+        "id": 1,
         "title": "El pozo de la ascensión",
         "author": "Brandon Sanderson",
         "shortDescription": "La segunda entrega de la aclamada serie Nacidos de la bruma",
@@ -31,10 +31,10 @@ const books = [
               }
           }
         ],
-        "comments": []
+        "comments": ["carcel"]
     },
     {
-        "id": 35,
+        "id": 2,
         "title": "El pozo de la ascensión",
         "author": "Brandon Sanderson",
         "shortDescription": "La segunda entrega de la aclamada serie Nacidos de la bruma",
@@ -55,7 +55,122 @@ const books = [
             }
         ],
         "comments": []
-    }
+    },
+    {
+      "id": 3,
+      "title": "El pozo de la ascensión",
+      "author": "Brandon Sanderson",
+      "shortDescription": "La segunda entrega de la aclamada serie Nacidos de la bruma",
+      "description": "Durante mil años el Lord Legislador reina con un poder absoluto gracias al terror, a sus poderes y a su inmortalidad. Pero vencer y matar al Lord Legislador fue la parte sencilla. El verdadero desafío será sobrevivir a las consecuencias de su caída.",
+      "votesCount": 12,
+      "createdAt": "2024-12-30T18:42:27.681Z",
+      "updatedAt": "2024-12-30T18:42:27.681Z",
+      "tags": [
+          {
+              "id": 1,
+              "name": "Fantasía",
+              "BookTag": {
+                  "createdAt": "2024-12-30T18:42:27.742Z",
+                  "updatedAt": "2024-12-30T18:42:27.742Z",
+                  "BookId": 35,
+                  "TagId": 1
+              }
+          }
+      ],
+      "comments": []
+  },
+  {
+    "id": 4,
+    "title": "El pozo de la ascensión",
+    "author": "Brandon Sanderson",
+    "shortDescription": "La segunda entrega de la aclamada serie Nacidos de la bruma",
+    "description": "Durante mil años el Lord Legislador reina con un poder absoluto gracias al terror, a sus poderes y a su inmortalidad. Pero vencer y matar al Lord Legislador fue la parte sencilla. El verdadero desafío será sobrevivir a las consecuencias de su caída.",
+    "votesCount": 12,
+    "createdAt": "2024-12-30T18:42:27.681Z",
+    "updatedAt": "2024-12-30T18:42:27.681Z",
+    "tags": [
+        {
+            "id": 1,
+            "name": "Fantasía",
+            "BookTag": {
+                "createdAt": "2024-12-30T18:42:27.742Z",
+                "updatedAt": "2024-12-30T18:42:27.742Z",
+                "BookId": 35,
+                "TagId": 1
+            }
+        }
+    ],
+    "comments": []
+},
+{
+  "id": 5,
+  "title": "El pozo de la ascensión",
+  "author": "Brandon Sanderson",
+  "shortDescription": "La segunda entrega de la aclamada serie Nacidos de la bruma",
+  "description": "Durante mil años el Lord Legislador reina con un poder absoluto gracias al terror, a sus poderes y a su inmortalidad. Pero vencer y matar al Lord Legislador fue la parte sencilla. El verdadero desafío será sobrevivir a las consecuencias de su caída.",
+  "votesCount": 12,
+  "createdAt": "2024-12-30T18:42:27.681Z",
+  "updatedAt": "2024-12-30T18:42:27.681Z",
+  "tags": [
+      {
+          "id": 1,
+          "name": "Fantasía",
+          "BookTag": {
+              "createdAt": "2024-12-30T18:42:27.742Z",
+              "updatedAt": "2024-12-30T18:42:27.742Z",
+              "BookId": 35,
+              "TagId": 1
+          }
+      }
+  ],
+  "comments": ["me gusto", "fachero"]
+},
+{
+  "id": 6,
+  "title": "El pozo de la ascensión",
+  "author": "Brandon Sanderson",
+  "shortDescription": "La segunda entrega de la aclamada serie Nacidos de la bruma",
+  "description": "Durante mil años el Lord Legislador reina con un poder absoluto gracias al terror, a sus poderes y a su inmortalidad. Pero vencer y matar al Lord Legislador fue la parte sencilla. El verdadero desafío será sobrevivir a las consecuencias de su caída.",
+  "votesCount": 12,
+  "createdAt": "2024-12-30T18:42:27.681Z",
+  "updatedAt": "2024-12-30T18:42:27.681Z",
+  "tags": [
+      {
+          "id": 1,
+          "name": "Fantasía",
+          "BookTag": {
+              "createdAt": "2024-12-30T18:42:27.742Z",
+              "updatedAt": "2024-12-30T18:42:27.742Z",
+              "BookId": 35,
+              "TagId": 1
+          }
+      }
+  ],
+  "comments": []
+},
+{
+  "id": 7,
+  "title": "El pozo de la ascensión",
+  "author": "Brandon Sanderson",
+  "shortDescription": "La segunda entrega de la aclamada serie Nacidos de la bruma",
+  "description": "Durante mil años el Lord Legislador reina con un poder absoluto gracias al terror, a sus poderes y a su inmortalidad. Pero vencer y matar al Lord Legislador fue la parte sencilla. El verdadero desafío será sobrevivir a las consecuencias de su caída.",
+  "votesCount": 12,
+  "createdAt": "2024-12-30T18:42:27.681Z",
+  "updatedAt": "2024-12-30T18:42:27.681Z",
+  "tags": [
+      {
+          "id": 1,
+          "name": "Fantasía",
+          "BookTag": {
+              "createdAt": "2024-12-30T18:42:27.742Z",
+              "updatedAt": "2024-12-30T18:42:27.742Z",
+              "BookId": 35,
+              "TagId": 1
+          }
+      }
+  ],
+  "comments": ["good"]
+},
 ]
 
 
@@ -67,58 +182,90 @@ const increment = 5; // Cuántos books mostrar por clic
 const bookList = document.getElementById("link-list");
 const moreButton = document.getElementById("ver-mas");
 
-// Función para cargar books
 function loadBooks() {
   const nextIndex = currentIndex + increment;
   const booksDisplayed = books.slice(currentIndex, nextIndex);
 
-  // Itera sobre los libros a mostrar
-  for (let i = 0; i < booksDisplayed.length; i++) {
-    const book = booksDisplayed[i];
-    const tags = book.tags;  // Accede correctamente a los tags
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    const p = document.createElement("p");
+  const fragment = document.createDocumentFragment();
 
-    // Define el enlace hacia el libro
-    a.href = `book.html?id=${book.id}`;
-    a.textContent = book.title;
+  booksDisplayed.forEach(book => {
+    const bookItem = createBookItem(book);
+    fragment.appendChild(bookItem);
+  });
 
-    // Descripción corta del libro
-    p.textContent = book.shortDescription;
-    p.classList.add("short-description");
-
-    // Crear un nuevo <ul> dentro de este <li> para los tags
-    const subUl = document.createElement("ul");
-    subUl.classList.add("tags-list");
-    const tagEmoji = document.createElement("li");
-    tagEmoji.textContent = "🏷️";
-    subUl.appendChild(tagEmoji);
-    // Crear elementos <li> dentro de este <ul> para cada tag
-    tags.forEach(tag => {
-      const subLi = document.createElement("li");
-      subLi.textContent = tag.name;
-      subLi.classList.add("tag-item")
-      subUl.appendChild(subLi);
-    });
-    // Agregar los subelementos al <ul>
-    li.appendChild(a);
-    li.appendChild(p);
-    li.appendChild(subUl);
-    bookList.appendChild(li);
-  }
-
-  // Actualizar el índice para la próxima carga
+  bookList.appendChild(fragment);
   currentIndex = nextIndex;
 
-  // Ocultar el botón si no hay más books por cargar
   if (currentIndex >= books.length) {
     moreButton.style.display = "none";
   }
 }
 
-// Evento para el botón "Ver más"
-moreButton.addEventListener("click", loadBooks);
+// Función para cargar books
+function createTagList(tags) {
+  const subUl = document.createElement("ul");
+  subUl.classList.add("tags-list");
 
-// Carga inicial de books
+  const tagEmoji = document.createElement("li");
+  tagEmoji.textContent = "🏷️";
+  subUl.appendChild(tagEmoji);
+
+  tags.forEach(tag => {
+    const subLi = document.createElement("li");
+    subLi.textContent = tag.name;
+    subLi.classList.add("tag-item");
+    subUl.appendChild(subLi);
+  });
+
+  return subUl;
+}
+
+function createVotesSection(votesCount, allComments) {
+  const divVotes = document.createElement("div");
+  divVotes.classList.add("votes");
+
+  const votes = document.createElement("p");
+  votes.textContent = "Votos:";
+  const amountVotes = document.createElement("p");
+  amountVotes.textContent = votesCount;
+  const comments = document.createElement("p");
+  comments.textContent = "Comentarios";
+  const amountComments = document.createElement("p");
+  amountComments.textContent = allComments.length;
+
+  divVotes.appendChild(votes);
+  divVotes.appendChild(amountVotes);
+  divVotes.appendChild(comments);
+  divVotes.appendChild(amountComments);
+  
+
+  return divVotes;
+}
+
+function createBookItem(book) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  const p = document.createElement("p");
+
+  a.href = `book.html?id=${book.id}`;
+  a.textContent = book.title;
+
+  p.textContent = book.shortDescription;
+  p.classList.add("short-description");
+
+  const tagList = createTagList(book.tags);
+  const votesSection = createVotesSection(book.votesCount, book.comments);
+
+  li.appendChild(a);
+  li.appendChild(p);
+  li.appendChild(votesSection);
+  li.appendChild(tagList);
+
+
+  return li;
+}
 loadBooks();
+moreButton.addEventListener("click", () => {
+  loadBooks(); 
+  
+});
